779aa7d38b236bfcc9a5ec4528cd00feda1c1715
Ian <ianperesdarosa2004@gmail.com>
2024-01-29 11:04:04 -0300

    commit push

--- codes/00.php
+++ codes/00.php
@@ -1,2 +1,26 @@
 <?php
-echo "Hello"
+
+class Person {
+    public $name;
+    public $age;
+
+
+    function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+
+    function get_age() {
+        return $this->age;
+    }
+
+}
+
+$logs = new Person("Ian", 19);
+echo $logs->get_name();
+echo $logs->get_age();
+?>
